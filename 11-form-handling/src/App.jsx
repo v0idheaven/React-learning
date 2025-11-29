ad03d545e368a55435adf5ca7ee4b256916c390d
v0idheaven <dahiyavarun2007@gmail.com>
2025-11-29 17:08:50 +0530

    form handling

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+
+const App = () => {
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
+    
+  }
+
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder='Enter your name'/>
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
