3af70cf1a315084272327e2b3072268df8f74281
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-02 00:36:42 +0530

    javascript logic in components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -73,8 +73,16 @@ function Menu() {
 }
 
 function Footer() {
+    const hour = new Date().getHours()
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+    console.log(isOpen);
+
+    // if(hour >= openHour && hour <= closeHour); else alert("We're currently closed")
+
   return (
-    <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
+    <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
   );
 
   // return React.createElement('footer', null, "We're currently open!")
