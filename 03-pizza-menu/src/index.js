64f096ca3ad4346988aa25f1e2494e5e2f639b96
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-02 00:30:34 +0530

    creating more components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -49,7 +49,22 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
@@ -57,13 +72,21 @@ function App() {
   );
 }
 
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
+  );
+
+  // return React.createElement('footer', null, "We're currently open!")
+}
+
 function Pizza() {
-  return(
-  <div>
-    <img src="pizzas/spinaci.jpg" alt="Pizza spinaci"/>
-    <h2>Pizza Spinaci</h2>
-    <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-  </div>
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
   );
 }
 
