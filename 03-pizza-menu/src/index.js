7f7726439b472e898dd1d15fc8a21cba4d1f3a50
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-02 09:44:17 +0530

    extracting jsx into a new components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -137,6 +137,15 @@ function Pizza(props) {
   );
 }
 
+function Order({ closeHour }) {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online!</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
@@ -150,7 +159,13 @@ function Footer() {
 
   if (!isOpen) {
     return (
-      <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00!</p>
+      <footer className="footer">
+        {isOpen ? (
+          <Order closeHour={closeHour} />
+        ) : (
+          <p>We're happy to see you! between {openHour}:00 and {closeHour}:00</p>
+        )}
+      </footer>
     );
   }
 
