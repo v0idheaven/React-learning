5ab814ebc4e2054ef269b7d3e4c9acbadb78c1eb
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-02 09:31:29 +0530

    conditional rendering with ternaries

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -77,7 +77,7 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas >  0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza
@@ -88,7 +88,7 @@ function Menu() {
             />
           ))}
         </ul>
-      )}
+      ) : <p>We're still working on our menu. Please come back later!</p>}
 
       {/* <ul className="pizzas">
         {pizzas.map((pizza) => (
@@ -144,11 +144,13 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online!</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>We're happy to see you! between {openHour}:00 and {closeHour}:00</p>
       )}
     </footer>
   );
