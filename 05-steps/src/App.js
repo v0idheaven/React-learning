275f9b6c5ef23de7b18cd71e0d3b0a30cc459d44
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-03 15:50:00 +0530

    steps updated

--- 05-steps/src/App.js
+++ 05-steps/src/App.js
@@ -7,6 +7,14 @@ const messages = [
 export default function App() {
   const step = 1;
 
+  function handlePrevious() {
+    alert('Prev')
+  }
+
+  function handleNext() {
+    alert('next')
+  }
+
   return <div className="steps">
     <div className="numbers">
       <div className={`${step >= 1 ? "active" : ""}`}>1</div>
@@ -17,10 +25,10 @@ export default function App() {
     <p className="message">Step {step}: {messages[step - 1]}</p>
 
     <div className="buttons">
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}}>
+      <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={() => handlePrevious}>
         Previous
       </button>
-      <button style={{backgroundColor: "#7950f2", color: "#fff"}}>
+      <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={() => handleNext}>
         Next
       </button>
     </div>
