b9732497fc1a6cc29885c72a211b9b04c3fc7e89
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-10 22:55:35 +0530

    updating state based on current state

--- 05-steps/src/App.js
+++ 05-steps/src/App.js
@@ -11,11 +11,11 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
 
     //BAD PRACTICE: DO NOT MUTATE THE STATE
     // test.name = "Fred"
