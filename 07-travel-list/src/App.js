9253891d6f85fc2c0ad53fc4ce8882c2157ab48b
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-11 12:39:00 +0530

    building the layout

---
+++ 07-travel-list/src/App.js
@@ -0,0 +1,28 @@
+export default function App() {
+    return <div className="app">
+        <Logo />
+        <Form />
+        <PackingList />
+        <Stats />
+    </div>
+}
+
+function Logo() {
+    return <h1>🌴 Far Away 💼</h1>
+}
+
+function Form() {
+    return <div className="add-form">
+        <h3>What do you nedd for your 😍 trip?</h3>
+    </div>
+}
+
+function PackingList() {
+    return <div className="list">LIST</div>
+}
+
+function Stats() {
+    return <footer className="stats">
+        <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+}
