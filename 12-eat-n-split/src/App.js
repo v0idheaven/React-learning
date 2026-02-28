e58827bb803e49dfeb3a7837fc2d55517a092291
v0idheaven <dahiyavarun2007@gmail.com>
2026-02-28 17:16:24 +0530

    displaying the new friend form

--- 12-eat-n-split/src/App.js
+++ 12-eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,13 +21,27 @@ const initialFriends = [
   },
 ];
 
+function Button({children, onClick}) {
+  return (
+    <button className="button" onClick={onClick}>{children}</button>
+  )
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={() => handleShowAddFriend()}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
@@ -58,12 +74,6 @@ function Friend({friend}) {
 
       <Button>Select</Button>
     </li>
-  )
-}
-
-function Button({children}) {
-  return (
-    <button className="button">{children}</button>
   )
 }
 
