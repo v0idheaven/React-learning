cd27206491c01ebc6d6169ac41676d336609f43e
v0idheaven <dahiyavarun2007@gmail.com>
2026-03-11 19:00:01 +0530

    eat n split

--- 12-eat-n-split/src/App.js
+++ 12-eat-n-split/src/App.js
@@ -78,16 +78,36 @@ function Friend({friend}) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?u=${id}`,
+      balance: 0,
+  };
+
+  setName("");
+  setImage("https://i.pravatar.cc/48");
+}
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
 
       <label>🧑‍🤝‍🧑 Friend name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
 
       <label>📷 Image URL</label>
-      <input type="text" />
+      <input type="text" value={image} onChange={e=>setImage(e.target.value)}/>
 
-      <Button>Create</Button>
+      <Button>Add</Button>
     </form>
   )
 }
